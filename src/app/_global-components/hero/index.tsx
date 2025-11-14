87c63f9d54d9af060ac2b13ab17b2271e3cfad76
Adetokunbo Ogunnoiki <adetokunboogunnoiki28@gmail.com>
2025-11-14 20:03:44 +0100

    fix : hero for the desktop view

--- src/app/_global-components/hero/index.tsx
+++ src/app/_global-components/hero/index.tsx
@@ -24,7 +24,7 @@ const Hero = () => {
         className="absolute top-0 left-0 w-full h-full object-cover -z-10"
       >
         {/* Make sure to place your video in the `public/videos` folder */}
-        <source src="public/videos/herovid.mp4" type="video/mp4" />
+        <source src="/videos/herovid.mp4" type="video/mp4" />
         Your browser does not support the video tag.
       </video>
 
@@ -33,15 +33,15 @@ const Hero = () => {
 
       {/* Existing Content */}
       <div className="relative z-10">
-        <div className="max-w-7xl mx-auto flex flex-col lg:flex-row justify-between items-center gap-8 lg:gap-20">
-          <div className="flex flex-col space-y-4 sm:space-y-6 w-full lg:w-[55%] max-w-xl">
-            <h1 className="text-3xl sm:text-4xl lg:text-5xl xl:text-6xl font-medium text-[#1F253F] leading-tight">
+        <div className="max-w-7xl  flex flex-col lg:flex-row justify-center items-center ">
+          <div className="flex flex-col space-y-4 sm:space-y-6 w-full lg:w-[55%] ">
+            <h1 className="text-3xl sm:text-4xl lg:text-5xl xl:text-6xl font-bold text-center text-[#F7F6F2] ">
               <TextAnimate animation="slideUp" by="word">
                 Reliable Power Solutions
               </TextAnimate>
             </h1>
 
-            <h1 className="text-[#6B7280] text-[14px] sm:text-lg font-[400]">
+            <h1 className="text-[#B1AD4E] text-center text-[14px] sm:text-lg font-[400]">
               <TextAnimate animation="fadeIn" by="line" as="p">
                 Bolton Engineering offers top-quality generator sales and rentals
                 across Southwest Nigeria. We ensure uninterrupted power for your
@@ -50,7 +50,7 @@ const Hero = () => {
               </TextAnimate>
             </h1>
 
-            <div className="flex flex-col sm:flex-row gap-4 sm:gap-6 pt-2">
+            <div className="flex flex-col sm:flex-row gap-4 sm:gap-6 pt-2 justify-center">
               <Button className="bg-[#1F253F] text-white px-6 sm:px-8 py-5 sm:py-6 rounded-2xl text-sm sm:text-base w-full sm:w-fit transition-colors duration-200">
                 Generator Sales
                 <svg
@@ -90,7 +90,7 @@ const Hero = () => {
             </div>
           </div>
 
-          <div className="relative  h-auto ">
+          {/* <div className="relative  h-auto ">
             <Image
               src="/images/heroImg.webp"
               alt="hero image"
@@ -100,7 +100,7 @@ const Hero = () => {
               className="object-contain"
               priority
             />
-          </div>
+          </div> */}
         </div>
         <div className="relative z-10 flex flex-col sm:flex-row items-center justify-center gap-4 sm:gap-8 mt-8 sm:mt-12">
           <p className="text-[16px] text-white/80 w-[176px] flex-wrap text-center sm:text-start">
@@ -112,7 +112,7 @@ const Hero = () => {
               alt="FG Wilson logo"
               width={100}
               height={525}
-              className="brightness-0 invert"
+              className=""
             />
             <Image
               src="/images/kohler-generators-logo.webp"
@@ -133,7 +133,7 @@ const Hero = () => {
               alt="Kohler Generators logo"
               width={100}
               height={225}
-              className="brightness-0 invert"
+              className=""
             />
             <KohlerGeneratorsLogo className="brightness-0 invert" />
             {/* <GrammalyLogo /> */}
